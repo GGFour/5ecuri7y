bd0d815745044c233e9d73b634683efa04dddc5e
Vladimir Surtaev <surtaevv+gh@inbox.ru>
2025-11-16 07:58:30 +0200

    back: fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -7,15 +7,15 @@ class Settings(BaseSettings):
     environment: str = "local"
     debug: bool = True
 
-    database_url: str = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
+    database_url: str = (
+        "postgresql+psycopg2://withsecure:security@31.22.104.110:5432/withsecure"
+    )
 
     n8n_webhook_url: str = "https://example.com/webhook/mock"
     n8n_timeout_seconds: int = 15
     n8n_jwt_token: str = "json.web.token"
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 @lru_cache
